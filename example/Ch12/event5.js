7123acc62262c06cf24a16b2d490c04df4a2d165
igor <mizikikou@gmail.com>
2026-07-28 10:47:17 +0800

    Initial commit

---
+++ example/Ch12/event5.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function() {
+  showMsg('Hello, world!');
+}, false);
+
+function showMsg(msg) {
+  window.alert(msg);
+}
---
+++ example/Ch12/mouseover.js
@@ -0,0 +1,9 @@
+let fig = document.getElementById('fig');
+
+fig.addEventListener('mouseover', function() {
+  fig.src = 'girl2.png';
+}, false);
+
+fig.addEventListener('mouseout', function() {
+  fig.src = 'girl1.png';
+}, false);
